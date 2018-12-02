70faf90ea334240960591930e8c2d15ae6b18cb0
tca <tcarland@gmail.com>
2018-12-02 10:01:58 -0800

    sync minor updates

--- tdh-mysql/bin/tdh-mysql-init.sh
+++ tdh-mysql/bin/tdh-mysql-init.sh
@@ -16,15 +16,14 @@ res=
 
 if [ -z "$name" ]; then
     name="tdh-mysql1"
-    echo "No parameters. Initializing Docker Mysql instance as ''$name'"
 fi
-
 if [ -z "$port" ]; then
     port=3307
     echo "Default local port set to $port"
 fi
-
 volname="${name}-vol1"
+
+echo "Initializing Docker container instance as '$name'"
 
 ( docker run --name $name -p${port}:3306 \
   --mount "type=bind,src=${tdh_path}/../etc/tdh-mysql.cnf,dst=/etc/my.cnf" \
@@ -34,8 +33,7 @@ volname="${name}-vol1"
   -d mysql/mysql-server:5.7 \
   --character-set-server=utf8 --collation-server=utf8_general_ci )
 
- res=$?
-
+res=$?
 if [ $res -ne 0 ]; then
     echo "ERROR in $PNAME, abort..."
     exit $res
@@ -43,7 +41,6 @@ fi
 
 #  initialization scripts
 # --mount type=bind,src=/path-on-host-machine/scripts/,dst=/docker-entrypoint-initdb.d/ \
-
 # allow mysqld to start and generate password
 sleep 3
 passwd=$( docker logs tdh-mysql1 2>&1 | grep GENERATED | awk -F': ' '{ print $2 }' )
