59bf05ef2532da56c7afa6f430af2ed9061ea04e
tca <tcarland@gmail.com>
2018-12-14 07:47:04 -0800

    add docker network support

--- tdh-mysql/bin/tdh-mysql-init.sh
+++ tdh-mysql/bin/tdh-mysql-init.sh
@@ -5,39 +5,95 @@
 #   Initialize MySQL Daemon via Docker
 #
 PNAME=${0##*\/}
-name="$1"
-port="$2"
-#network="$3"
 tdh_path=$(dirname "$(readlink -f "$0")")
+name="tdh-mysql1"
+port="3306"
+network=
 volname=
 res=
+ACTION=
 
+usage()
+{
+    echo ""
+    echo "Usage: $PNAME [options] run|start"
+    echo "   -h|--help             = Display usage and exit."
+    echo "   -N|--network <name>   = Attach container to Docker network"
+    echo "   -n|--name <name>      = Name of the Docker Container instance."
+    echo "   -p|--port <port>      = Local bind port for the container."
+    echo " Any other action than 'run|start' results in a dry run."
+    echo " The container will only start with the run or start action"
+}
 
-if [ -z "$name" ]; then
-    name="tdh-mysql1"
+while [ $# -gt 0 ]; do
+    case "$1" in
+        -h|--help)
+            usage
+            exit 0
+            ;;
+        -N|--network)
+            network="$2"
+            shift
+            ;;
+        -n|--name)
+            name="$2"
+            shift
+            ;;
+        -p|--port)
+            port="$2"
+            shift
+            ;;
+        *)
+            ACTION="$1"
+            shift
+            ;;
+    esac
+    shift
+done
+
+if [ -z "$ACTION" ]; then 
+    usage
 fi
 
-if [ -z "$port" ]; then
-    port=3307
-    echo "Host local port set to $port"
+volname="${name}-data1"
+
+cmd="docker run --name $name -p $port:3306 -d"
+
+if [ -n "$network" ]; then 
+    cmd="$cmd --network $network"
 fi
 
-volname="${name}-vol1"
-
-echo "Initializing Docker container instance as '$name'"
-echo "    with volume '$volname'"
-
-
-( docker run --name $name -p${port}:3306 -d \
-  --mount "type=bind,src=${tdh_path}/../etc/tdh-mysql.cnf,dst=/etc/my.cnf" \
-  --mount "type=volume,source=${volname},target=/var/lib/mysql" \
-  --env MYSQL_RANDOM_ROOT_PASSWORD=true \
-  --env MYSQL_LOG_CONSOLE=true \
-  mysql/mysql-server:5.7 \
-  --character-set-server=utf8 --collation-server=utf8_general_ci )
-
+cmd="$cmd --mount \"type=bind,src=${tdh_path}/../etc/tdh-mysql.cnf,dst=/etc/my.cnf\" \
+--mount \"type=volume,source=${volname},target=/var/lib/mysql\" \
+--env MYSQL_RANDOM_ROOT_PASSWORD=true \
+--env MYSQL_LOG_CONSOLE=true \
+mysql/mysql-server:5.7 \
+--character-set-server=utf8 --collation-server=utf8_general_ci"
 #  initialization scripts
 # --mount type=bind,src=/path-on-host-machine/scripts/,dst=/docker-entrypoint-initdb.d/ \
+
+echo ""
+echo "  TDH Docker Container: '$name'"
+echo "  Container Volume Name: $volume"
+echo "  Local port: $port"
+echo "" 
+
+ACTION=$(echo $ACTION | tr [:upper:] [:lower:])
+
+if [ "$ACTION" == "run" ] || [ "$ACTION" == "start" ]; then
+    echo "Starting container '$name'"
+
+    ( $cmd )
+
+    # allow mysqld to start and generate password
+    sleep 3
+    passwd=$( docker logs tdh-mysql1 2>&1 | grep GENERATED | awk -F': ' '{ print $2 }' )
+    echo "passwd='$passwd'"
+else
+    echo "  <DRYRUN> - Command to exec would be: "; echo ""
+    echo "( $cmd )"
+    echo ""
+fi
 
 res=$?
 
@@ -45,10 +101,5 @@ if [ $res -ne 0 ]; then
     echo "ERROR in run for $PNAME"
     exit $res
 fi
-
-# allow mysqld to start and generate password
-sleep 3
-passwd=$( docker logs tdh-mysql1 2>&1 | grep GENERATED | awk -F': ' '{ print $2 }' )
-echo "passwd='$passwd'"
 
 exit $res
