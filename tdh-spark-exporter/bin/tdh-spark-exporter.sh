6cf422744e39cdcebe085d07ebcdb246db575866
tca <tcarland@gmail.com>
2019-02-13 10:33:38 -0800

    fix error in cmd assignment; fix usage

--- tdh-spark-exporter/bin/tdh-spark-exporter.sh
+++ tdh-spark-exporter/bin/tdh-spark-exporter.sh
@@ -7,6 +7,7 @@
 PNAME=${0##*\/}
 tdh_path=$(dirname "$(readlink -f "$0")")
 name="tdh-spark-exporter1"
+port="9109"
 network=
 res=
 ACTION=
@@ -18,10 +19,8 @@ usage()
     echo ""
     echo "Usage: $PNAME [options] run|start"
     echo "   -h|--help             = Display usage and exit."
-    echo "   -N|--network <name>   = Attach container to Docker network"
     echo "   -n|--name <name>      = Name of the Docker Container instance."
-    echo "   -p|--port <port>      = Local bind port for the container (default=${port})."
-    echo "   -v|--volume <name>    = Optional volume name. Defaults to \$name-data"
+    echo "   -N|--network <name>   = Attach container to Docker network"
     echo ""
     echo "Any other action than 'run|start' results in a dry run."
     echo "The container will only start with the run or start action"
@@ -61,18 +60,6 @@ while [ $# -gt 0 ]; do
             network="$2"
             shift
             ;;
-        -p|--port)
-            port="$2"
-            shift
-            ;;
-        -R|--rmhost)
-            rmhost="$2"
-            shift
-            ;;
-        -P|--rmport)
-            rmport="$2"
-            shift 
-            ;;
         *)
             ACTION="$1"
             shift
@@ -89,19 +76,18 @@ cmd="docker run --name $name -d"
 
 if [ -n "$network" ]; then
     validate_network "$network"
-    cmd="$cmd -p 9108:9108 -p 9109:9109 -p 9109:9109/udp"
+    cmd="$cmd -p 9108:9108 -p $port:9109 -p $port:9109/udp"
 else 
     network="host"
 fi
 
 cmd="$cmd --network ${network}"
 cmd="$cmd --mount type=bind,src=${tdh_path}/../etc/graphite_mapping.conf,dst=/tmp/graphite_mapping.conf"
-cdm="$cmd prom/graphite-exporter --graphite.mapping-config=/tmp/graphite_mapping.conf" 
+cmd="$cmd prom/graphite-exporter --graphite.mapping-config=/tmp/graphite_mapping.conf" 
 
 
 echo ""
 echo "  TDH Docker Container: '$name'"
-echo "  Container Volume name: $volname"
 echo "  Network: $network"
 echo "  Local port: $port"
 echo ""
