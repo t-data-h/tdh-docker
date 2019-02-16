4949a48c61690e7a0b33e75856485501a65b9669
tca <tcarland@gmail.com>
2019-02-16 12:41:32 -0800

    add fronting script

---
+++ tdh/bin/tdh.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+#
+PNAME=${0##*\/}
+tdh_path=$(dirname "$(readlink -f "$0")")
+
+
+usage()
+{
+    echo "Usage: tdh.sh [start|stop|status]"
+}
+
+
+while [ $# -gt 0 ]; do
+    case "$1" in
+        start)
+            ( $tdh_path/tdh-run-all.sh start )
+            ;;
+        stop)
+            ( $tdh_path/tdh-run-all.sh stop )
+            ;;
+        list|status)
+            ( docker container list --all --filter name="tdh" )
+            ;;
+        *)
+            usage
+            ;;
+    esac
+    shift
+done
+
+exit 0
