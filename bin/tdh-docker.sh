af8b001885c1cba4ecde5a57c47103f67086b1fd
tca <tcarland@gmail.com>
2019-07-27 16:13:20 -0700

    add version

--- bin/tdh-docker.sh
+++ bin/tdh-docker.sh
@@ -1,24 +1,32 @@
 #!/bin/bash
-#
+#  TDH Docker Container entry point for starting/stopping containers
 #
 PNAME=${0##*\/}
 tdh_path=$(dirname "$(readlink -f "$0")")
 prefix="tdh"
 runscript="$tdh_path/tdh-docker-run.sh"
+version="v0.2.9"
 
 usage()
 {
     echo "Usage: $PNAME [start|stop|status]"
 }
 
+version()
+{
+    echo "$PNAME: $version"
+}
+
+
 if [ $# -eq 0 ]; then
     usage
     exit 1
 fi
 
+
 while [ $# -gt 0 ]; do
     case "$1" in
-        --prefix)
+        -p|--prefix)
             prefix="$2"
             shift
             ;;
@@ -31,6 +39,9 @@ while [ $# -gt 0 ]; do
         list|status)
             ( docker container list --all --filter name="$prefix" )
             ;;
+        -V|--version)
+            version
+            exit 0
         *)
             usage
             ;;
